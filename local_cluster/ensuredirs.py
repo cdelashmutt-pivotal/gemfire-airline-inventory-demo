2c2a7b6a46558b6bfbb9dbea37fa8eb0c76a5083
wrmay <rmay@pivotal.io>
2017-03-22 23:06:03 -0400

    add local cluster scripts and config

---
+++ local_cluster/ensuredirs.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2015-2016 Pivotal Software, Inc. All Rights Reserved.
+#
+import os
+import os.path
+import sys
+
+assert sys.version_info > (3,0)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        sys.exit('no arguments provided. please provide one or more directory names')
+
+    for dname in sys.argv[1:]:
+        if os.path.exists(dname):
+            if os.path.isfile(dname):
+                sys.exit('cannot create directory {0} because a file with the same name already exists'.format(dname))
+        else:
+            os.makedirs(dname)
+            print('created directory {0}'.format(dname))
